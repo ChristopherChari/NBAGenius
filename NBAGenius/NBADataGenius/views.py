e28eb267173cfd59d3a337c335a72f2b66c9d2ef
christopherchari.dev@gmail.com <christopherchari.dev@gmail.com>
2024-02-27 18:28:36 +0000

    print advanced stats

--- NBAGenius/NBADataGenius/views.py
+++ NBAGenius/NBADataGenius/views.py
@@ -49,17 +49,21 @@ def player_list(request):
 
 
 from nba_api.stats.endpoints.playercareerstats import PlayerCareerStats
+from nba_api.stats.endpoints.playerdashboardbyyearoveryear import PlayerDashboardByYearOverYear
+
 def player_profile(request, player_id):
     try:
         # Retrieve player information from the NBA API
         player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id)
 
-        # Retrieve player career stats from the NBA API
+        # Retrieve player career stats from the NBA API with measure type set to 'Advanced'
         player_dashboard = PlayerDashboardByYearOverYear(player_id=player_id)
+        player_dashboard_Advanced = PlayerDashboardByYearOverYear(player_id=player_id, measure_type_detailed='Advanced')
 
         # Convert the responses to dictionary format
         player_info_dict = player_info.get_normalized_dict()
         player_dashboard_dict = player_dashboard.get_normalized_dict()
+        player_dashboard_advanced_dict = player_dashboard_Advanced.get_normalized_dict()
 
         # Check if player data is found
         if player_info_dict and 'CommonPlayerInfo' in player_info_dict:
@@ -82,27 +86,35 @@ def player_profile(request, player_id):
                 # Calculate Stats/GP
                 for season_stats in player_dashboard_data:
                     if season_stats['GP'] != 0:
-                        season_stats['MIN_PER_GP'] = round(season_stats['MIN'] / season_stats['GP'],1)
+                        season_stats['MIN_PER_GP'] = round(season_stats['MIN'] / season_stats['GP'], 1)
 
                         for key in season_stats:
-                                    if key in ['FG_PCT', 'FG3_PCT', 'FT_PCT']:
-                                        season_stats[key] = round(season_stats[key] *100 ,1)
-                        
+                            if key in ['FG_PCT', 'FG3_PCT', 'FT_PCT']:
+                                season_stats[key] = round(season_stats[key] * 100, 1)
+
                         for key in season_stats:
-                            if key in ['FGM', 'FGA', 'FG3M', 'FG3A', 'FTM', 'FTA', 'OREB', 'DREB', 'REB', 'AST', 'TOV', 'STL', 'BLK', 'BLKA', 'PF', 'PFD', 'PTS',]:
+                            if key in ['FGM', 'FGA', 'FG3M', 'FG3A', 'FTM', 'FTA', 'OREB', 'DREB', 'REB', 'AST',
+                                       'TOV', 'STL', 'BLK', 'BLKA', 'PF', 'PFD', 'PTS']:
                                 season_stats[key] = round(season_stats[key] / season_stats['GP'], 1)
-                                
                     else:
                         season_stats['MIN_PER_GP'] = 0
-                    
 
-                    
+            # Extract advanced stats
+            advanced_stats = None
+            if  player_dashboard_advanced_dict and 'ByYearPlayerDashboard' in  player_dashboard_advanced_dict:
+                advanced_stats =  player_dashboard_advanced_dict['ByYearPlayerDashboard']
 
-            # Pass the player data, headline stats, and dashboard stats to the template context
+            # Print advanced stats to console
+            if advanced_stats:
+                print("Advanced Stats:")
+                print(advanced_stats)
+
+            # Pass the player data, headline stats, dashboard stats, and advanced stats to the template context
             context = {
                 'player': player_data,
                 'player_stats': player_stats,
-                'player_dashboard': player_dashboard_data
+                'player_dashboard': player_dashboard_data,
+                'advanced_stats': advanced_stats
             }
             return render(request, 'player_profile.html', context)
         else:
